0e724bf668575c567422a625f829f1cc353423d8
OgabekMurodullaev <148078268+OgabekMurodullaev@users.noreply.github.com>
2025-02-12 13:24:08 +0500

    last changes

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.9-bullseye
+
+ENV PYTHONUNBUFFERED=1
+ENV PYTHONDONTWRITEBYTECODE=1
+
+WORKDIR /app
+
+RUN pip install --upgrade pip
+
+COPY requirements.txt .
+
+RUN python -m venv .venv && \
+    .venv/bin/pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 8000
+
+CMD [".venv/bin/python", "manage.py", "runserver", "0.0.0.0:8000"]
--- core/settings.py
+++ core/settings.py
@@ -104,12 +104,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('POSTGRES_DB', 'quiz_db'),
+#         'USER': os.getenv('POSTGRES_USER', 'quiz_user'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'quiz_password'),
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
