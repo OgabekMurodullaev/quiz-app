b749dfe80ec2dd5b037391599c3f31810c1fe883
OgabekMurodullaev <148078268+OgabekMurodullaev@users.noreply.github.com>
2025-02-14 21:35:11 +0500

    modified users app: tests

--- users/tests.py
+++ users/tests.py
@@ -35,3 +35,36 @@ class UserRegisterAPITestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("A user with that username already exists.", response.data["username"])
 
+    def test_register_weak_password(self):
+        response = self.client.post(self.register_url, self.invalid_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("This password is too short. It must contain at least 8 characters.", response.data["password"])
+
+
+class UserLoginAPITestCase(APITestCase):
+
+    def setUp(self):
+        self.login_url = "/users/login/"
+
+        self.user = User.objects.create(username="ogabek")
+        self.user.set_password("yu45#old")
+        self.user.save()
+
+    def test_login_success(self):
+        response = self.client.post(self.login_url, data={
+            "username": "ogabek",
+            "password": "yu45#old"
+        })
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("You have successfully logged in", response.data["message"])
+
+
+    def test_login_error(self):
+        response = self.client.post(self.login_url, data={
+            "username": "ogabek",
+            "password": "yu45#old1"
+        })
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("Username yoki parol noto'g'ri", response.data["non_field_errors"])
